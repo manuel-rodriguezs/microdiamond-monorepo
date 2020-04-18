3eb72b22919065bbe8e1d24a07d98d0e9dc79a94
Manuel Rodríguez <manuel.rodriguezs@gmail.com>
2020-04-18 20:57:41 +0200

    Configurations for native image compilation

--- buildnatives.sh
+++ buildnatives.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+cd commons
+./mvnw clean install
+status=$?
+cd ..
+if test $status -ne 0
+then
+    exit $status
+fi
 for d in microdiamond-server*/ ; do
     cd "$d"
     ./mvnw clean package -Pnative -Dquarkus.native.container-build=true
